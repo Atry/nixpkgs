bc477e06571b367e098bc64e72f1fce6f24b8d6d
Yang, Bo <pop.atry@gmail.com>
2022-04-30 07:40:57 +0000

    libdwarf_20210528: init at 20210528

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18306,6 +18306,7 @@ with pkgs;
   inherit (callPackage ../development/libraries/libdwarf { })
     libdwarf dwarfdump;
   libdwarf_0_4 = callPackage ../development/libraries/libdwarf/0.4.nix { };
+  libdwarf_20210528 = callPackage ../development/libraries/libdwarf/20210528.nix { };
 
   libe57format = callPackage ../development/libraries/libe57format { };
 
